d5d53f53d110dad963b013a2f9dd1174fad1e69b
4ked <max.goeke0@gmail.com>
2020-04-24 22:07:35 -0500

    spotify req working

--- spotify.py
+++ spotify.py
@@ -9,21 +9,41 @@ util.prompt_for_user_token('max.goeke',
                            client_secret='a5ea54deba2c49f2bb50382ba19c4d05',
                            redirect_uri='http://maxgoeke.me/jarvis')
 
-scope = 'user-library-read'
+# A few notes...
+# As of now you have to use environment variables, I'm still figuring this out
+# python3 spotify.py username \\ to run this script that prints all songs in every playlist of user username
 
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-
-token = util.prompt_for_user_token(username, scope)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
         track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name'])
-else:
-    print("Can't get token for", username)
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Whoops, need your username!")
+        print("usage: python user_playlists.py [username]")
+        sys.exit()
+
+    token = util.prompt_for_user_token(username)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print()
+                print(playlist['name'])
+                print ('  total tracks', playlist['tracks']['total'])
+                results = sp.playlist(playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    show_tracks(tracks)
+    else:
+        print("Can't get token for", username)
